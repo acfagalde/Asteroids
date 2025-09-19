ef7be8b3ea6c277ae9ab0ba5db2e3ec457afac52
Aaron <afagalde@gmail.com>
2025-09-18 21:18:14 -0500

    Added player, and movement physics

--- main.py
+++ main.py
@@ -1,16 +1,26 @@
-import pygame
+import pygame # type: ignore
 from constants import *
+from player import *
+from CircleShape import *
 
 def main():
     pygame.init()
+    framerate = pygame.time.Clock()
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    player1 = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
 
     while screen != None:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         pygame.Surface.fill(screen, (0,0,0))
+        player1.draw(screen)
+        player1.update(dt)
         pygame.display.flip()
+        dt = framerate.tick(60)/1000
+
+
 
 
     print("Starting Asteroids!")
